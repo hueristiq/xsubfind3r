c5838c92cc50004b9df6d7d304ef553ab323baae
Alex (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2025-06-23 04:02:08 -0400

    chore(mod): go get -f -t -u ./...

--- cmd/xsubfind3r/main.go
+++ cmd/xsubfind3r/main.go
@@ -55,7 +55,7 @@ func init() {
 	pflag.BoolVarP(&verbose, "verbose", "v", false, "")
 
 	pflag.Usage = func() {
-		hqgologger.Info().Label("").Msg(configuration.BANNER(au))
+		hqgologger.Info(configuration.BANNER(au), hqgologger.WithLabel(""))
 
 		h := "USAGE:\n"
 		h += fmt.Sprintf(" %s [OPTIONS]\n", configuration.NAME)
@@ -86,23 +86,25 @@ func init() {
 		h += " -s, --silent bool                    stdout in silent mode\n"
 		h += " -v, --verbose bool                   stdout in verbose mode\n"
 
-		hqgologger.Info().Label("").Msg(h)
-		hqgologger.Print().Msg("")
+		hqgologger.Info(h, hqgologger.WithLabel(""))
+		hqgologger.Print("")
 	}
 
 	pflag.Parse()
 
 	if err := configuration.CreateOrUpdate(configurationFilePath); err != nil {
-		hqgologger.Fatal().Msg(err.Error())
+		hqgologger.Fatal("failed creating or updating Configuration!", hqgologger.WithError(err))
 	}
 
 	viper.SetConfigFile(configurationFilePath)
+
 	viper.AutomaticEnv()
+
 	viper.SetEnvPrefix(strings.ToUpper(configuration.NAME))
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	if err := viper.ReadInConfig(); err != nil {
-		hqgologger.Fatal().Msg(err.Error())
+		hqgologger.Fatal("failed reading in Configuration!", hqgologger.WithError(err))
 	}
 
 	hqgologger.DefaultLogger.SetFormatter(
@@ -123,18 +125,18 @@ func init() {
 }
 
 func main() {
-	hqgologger.Info().Label("").Msg(configuration.BANNER(au))
+	hqgologger.Info(configuration.BANNER(au), hqgologger.WithLabel(""))
 
 	var cfg *configuration.Configuration
 
 	if err := viper.Unmarshal(&cfg); err != nil {
-		hqgologger.Fatal().Msg(err.Error())
+		hqgologger.Fatal("failed unmarshalling Configuration!", hqgologger.WithError(err))
 	}
 
 	if listSupportedSources {
-		hqgologger.Info().Msgf("listing, %v, current supported sources.", au.Underline(strconv.Itoa(len(cfg.Sources))).Bold())
-		hqgologger.Info().Msgf("sources marked with %v take in key(s) or token(s).", au.Underline("*").Bold())
-		hqgologger.Print().Msg("")
+		hqgologger.Info(fmt.Sprintf("listing, %v, current supported sources.", au.Underline(strconv.Itoa(len(cfg.Sources))).Bold()))
+		hqgologger.Info(fmt.Sprintf("sources marked with %v take in key(s) or token(s).", au.Underline("*").Bold()))
+		hqgologger.Print("")
 
 		needsKey := make(map[string]interface{})
 		keysElem := reflect.ValueOf(&cfg.Keys).Elem()
@@ -147,13 +149,13 @@ func main() {
 			source := cfg.Sources[index]
 
 			if _, ok := needsKey[source]; ok {
-				hqgologger.Print().Msgf("> %s *", source)
+				hqgologger.Print("> " + source + " *")
 			} else {
-				hqgologger.Print().Msgf("> %s", source)
+				hqgologger.Print("> " + source)
 			}
 		}
 
-		hqgologger.Print().Msg("")
+		hqgologger.Print("")
 
 		os.Exit(0)
 	}
@@ -161,7 +163,7 @@ func main() {
 	if domainsFilePath != "" {
 		file, err := os.Open(domainsFilePath)
 		if err != nil {
-			hqgologger.Error().Msg(err.Error())
+			hqgologger.Fatal("failed opening input file", hqgologger.WithError(err))
 		}
 
 		scanner := bufio.NewScanner(file)
@@ -175,7 +177,7 @@ func main() {
 		}
 
 		if err := scanner.Err(); err != nil {
-			hqgologger.Error().Msg(err.Error())
+			hqgologger.Fatal("failed reading input file!", hqgologger.WithError(err))
 		}
 
 		file.Close()
@@ -193,7 +195,7 @@ func main() {
 		}
 
 		if err := scanner.Err(); err != nil {
-			hqgologger.Error().Msg(err.Error())
+			hqgologger.Fatal("failed reading stdin!", hqgologger.WithError(err))
 		}
 	}
 
@@ -212,7 +214,7 @@ func main() {
 		Keys:             cfg.Keys,
 	})
 	if err != nil {
-		hqgologger.Fatal().Msg(err.Error())
+		hqgologger.Fatal("failed creating finder!", hqgologger.WithError(err))
 
 		return
 	}
@@ -220,8 +222,8 @@ func main() {
 	for index := range domains {
 		domain := domains[index]
 
-		hqgologger.Info().Msgf("Finding subdomains for %v...", au.Underline(domain).Bold())
-		hqgologger.Print().Msg("")
+		hqgologger.Info(fmt.Sprintf("Finding subdomains for %v...", au.Underline(domain).Bold()))
+		hqgologger.Print("")
 
 		outputs := []io.Writer{
 			os.Stdout,
@@ -233,14 +235,16 @@ func main() {
 		case outputFilePath != "":
 			file, err = writer.CreateFile(outputFilePath)
 			if err != nil {
-				hqgologger.Error().Msg(err.Error())
+				hqgologger.Fatal("failed craeting output file!", hqgologger.WithError(err), hqgologger.WithString("file", outputFilePath))
 			}
 
 			outputs = append(outputs, file)
 		case outputDirectoryPath != "":
-			file, err = writer.CreateFile(filepath.Join(outputDirectoryPath, domain))
+			outputFilePath = filepath.Join(outputDirectoryPath, domain)
+
+			file, err = writer.CreateFile(outputFilePath)
 			if err != nil {
-				hqgologger.Error().Msg(err.Error())
+				hqgologger.Fatal("failed craeting output file!", hqgologger.WithError(err), hqgologger.WithString("file", outputFilePath))
 			}
 
 			outputs = append(outputs, file)
@@ -253,11 +257,11 @@ func main() {
 				switch result.Type {
 				case sources.ResultError:
 					if verbose {
-						hqgologger.Error().Msgf("%s: %s", result.Source, result.Error)
+						hqgologger.Error("error finding subdomains!", hqgologger.WithError(err), hqgologger.WithString("source", result.Source))
 					}
 				case sources.ResultSubdomain:
 					if err := writer.Write(output, domain, result); err != nil {
-						hqgologger.Error().Msg(err.Error())
+						hqgologger.Error("error writing subdomain!", hqgologger.WithError(err), hqgologger.WithString("source", result.Source))
 					}
 				}
 			}
@@ -265,6 +269,6 @@ func main() {
 
 		file.Close()
 
-		hqgologger.Print().Msg("")
+		hqgologger.Print("")
 	}
 }
